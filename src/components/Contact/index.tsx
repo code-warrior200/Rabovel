6ab5412813cb482eb0a62a87f29b527211254b5a
Code~warrior200 <61137642+code-warrior200@users.noreply.github.com>
2024-10-14 08:01:58 -0700

    git commit -m

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -67,7 +67,7 @@ const Contact = () => {
                   </div>
                   <div className="w-full px-4">
                     <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
-                      Submit Ticket
+                      Send
                     </button>
                   </div>
                 </div>
